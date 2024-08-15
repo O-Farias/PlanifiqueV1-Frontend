3a739913f62cdb7f55d53d8b7027199352e416bc
Mateus Farias <mateus.f2001@hotmail.com>
2024-08-14 23:42:26 -0300

    Criado Context para Sidebar

---
+++ src/contexts/SidebarContext.tsx
@@ -0,0 +1,32 @@
+import React, { createContext, useState, useContext } from "react";
+
+interface SidebarContextType {
+  isOpen: boolean;
+  toggleSidebar: () => void;
+}
+
+const SidebarContext = createContext<SidebarContextType | undefined>(undefined);
+
+export const SidebarProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [isOpen, setIsOpen] = useState(true); // Inicialmente aberto
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <SidebarContext.Provider value={{ isOpen, toggleSidebar }}>
+      {children}
+    </SidebarContext.Provider>
+  );
+};
+
+export const useSidebar = () => {
+  const context = useContext(SidebarContext);
+  if (context === undefined) {
+    throw new Error("useSidebar must be used within a SidebarProvider");
+  }
+  return context;
+};
