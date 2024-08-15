c7f66ee6f3e62dcd3cc8a97c2322f35357a8d8f8
Mateus Farias <mateus.f2001@hotmail.com>
2024-08-14 23:42:05 -0300

    Criado nova página de categorias

---
+++ src/pages/Categorias.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  CssBaseline,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import Sidebar from "../components/Sidebar";
+import { useNavigate } from "react-router-dom";
+import { useSidebar } from "../contexts/SidebarContext";
+
+const drawerWidth = 240;
+const primaryColor = "#FF0000"; // Cor vermelha para corresponder ao ícone
+
+const Categorias: React.FC = () => {
+  const { isOpen, toggleSidebar } = useSidebar();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    // lógica para limpar o estado de autenticação
+    navigate("/login");
+  };
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{
+          backgroundColor: primaryColor,
+          zIndex: (theme) => theme.zIndex.drawer + 1,
+          transition: (theme) =>
+            theme.transitions.create(["width", "margin"], {
+              easing: theme.transitions.easing.sharp,
+              duration: theme.transitions.duration.leavingScreen,
+            }),
+          ...(isOpen && {
+            marginLeft: drawerWidth,
+            width: `calc(100% - ${drawerWidth}px)`,
+            transition: (theme) =>
+              theme.transitions.create(["width", "margin"], {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.enteringScreen,
+              }),
+          }),
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={toggleSidebar}
+            edge="start"
+            sx={{
+              marginRight: 5,
+              ...(isOpen && { display: "none" }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Categorias
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Sidebar drawerWidth={drawerWidth} onLogout={handleLogout} />
+
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
+        <Typography paragraph>
+          Bem-vindo à página de Categorias! O conteúdo das categorias vai aqui.
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default Categorias;
