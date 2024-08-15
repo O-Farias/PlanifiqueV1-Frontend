83f6c0a9d6d0dd58d61d818b202d72b88553ebaf
Mateus Farias <mateus.f2001@hotmail.com>
2024-08-14 23:41:47 -0300

    Incluído Context e Provider, para que o comportamento da Sidebar englobe toda a aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { SidebarProvider } from "./contexts/SidebarContext";
 import Login from "./components/auth/Login";
 import Cadastro from "./components/auth/Cadastro";
 import RecuperarSenha from "./components/auth/RecSenha";
 import Dashboard from "./pages/Dashboard";
+import Categorias from "./pages/Categorias";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/cadastro" element={<Cadastro />} />
-          <Route path="/recuperar-senha" element={<RecuperarSenha />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </div>
-    </Router>
+    <SidebarProvider>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/recuperar-senha" element={<RecuperarSenha />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/categorias" element={<Categorias />} />
+          </Routes>
+        </div>
+      </Router>
+    </SidebarProvider>
   );
 };
 
