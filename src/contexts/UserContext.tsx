3d78a5951c82a4b40e1ef80d4fdc5390f85d29f6
Mateus Farias <mateus.f2001@hotmail.com>
2024-08-20 14:28:00 -0300

    Alterado mensagem de erro

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -22,7 +22,7 @@ export const UserProvider: React.FC<{ children: React.ReactNode }> = ({
 export const useUser = () => {
   const context = useContext(UserContext);
   if (context === undefined) {
-    throw new Error("useUser must be used within a UserProvider");
+    throw new Error("useUser deve ser usado dentro de um UserProvider");
   }
   return context;
 };
