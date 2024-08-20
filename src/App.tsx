a2580ae057e89b194858baa2e338cc41397b9f26
Mateus Farias <mateus.f2001@hotmail.com>
2024-08-19 22:35:25 -0300

    Adicionado UserProvider, para que o Avatar no AppBar apareça em toda a aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { SidebarProvider } from "./contexts/SidebarContext";
+import { UserProvider } from "./contexts/UserContext";
 import Login from "./components/auth/Login";
 import Cadastro from "./components/auth/Cadastro";
 import RecuperarSenha from "./components/auth/RecSenha";
@@ -11,19 +12,21 @@ import Perfil from "./pages/Perfil";
 const App: React.FC = () => {
   return (
     <SidebarProvider>
-      <Router>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/cadastro" element={<Cadastro />} />
-            <Route path="/recuperar-senha" element={<RecuperarSenha />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/categorias" element={<Categorias />} />
-            <Route path="/perfil" element={<Perfil />} />
-          </Routes>
-        </div>
-      </Router>
+      <UserProvider>
+        <Router>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/recuperar-senha" element={<RecuperarSenha />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/categorias" element={<Categorias />} />
+              <Route path="/perfil" element={<Perfil />} />
+            </Routes>
+          </div>
+        </Router>
+      </UserProvider>
     </SidebarProvider>
   );
 };
