c58e3851bd3854b730e485d8a84c6cd32453d2c1
Mateus Farias <mateus.f2001@hotmail.com>
2024-09-02 22:14:39 -0300

    Removido comentários desnecessários

--- src/contexts/SidebarContext.tsx
+++ src/contexts/SidebarContext.tsx
@@ -1,21 +1,17 @@
 import React, { createContext, useState, useContext } from "react";
 
-// Definindo o tipo do contexto SidebarContextType
 interface SidebarContextType {
   isOpen: boolean;
   toggleSidebar: () => void;
 }
 
-//contexto SidebarProvider e o hook useSidebar
 const SidebarContext = createContext<SidebarContextType | undefined>(undefined);
 
-//  Provider SidebarProvider que irá prover o contexto SidebarContext
 export const SidebarProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const [isOpen, setIsOpen] = useState(true); // Inicialmente aberto
+  const [isOpen, setIsOpen] = useState(true);
 
-  // Definindo o valor do contexto e o método toggleSidebar
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
   };
